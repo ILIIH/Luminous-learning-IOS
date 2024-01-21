54296ecd72923c1a061019f0c377c0fd95ddfa67
Illia Branchuk <illiabranchuk@Illias-MacBook-Air.local>
2024-01-21 21:01:10 +0000

    init commit

---
+++ LumLearning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LumLearning
+//
+//  Created by Illia Branchuk on 20/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LumLearning/LumLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumLearningApp.swift
+//  LumLearning
+//
+//  Created by Illia Branchuk on 20/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct LumLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
